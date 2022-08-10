cedd10a374e0f4994b9e7d52b5ccee39cc0306ad
McOtech <otienopaul36@gmail.com>
2022-08-10 18:44:54 +0300

    simple frontend commit

--- scripts/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash 
-
-cargo build --all --target wasm32-unknown-unknown --release
-cp target/wasm32-unknown-unknown/release/*.wasm ./res/
--- scripts/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash 
-
-source ./scripts/setting.conf
-
-# near delete $SUB_ACCOUNT $MASTER_ACCOUNT 
-
-near create-account $SUB_ACCOUNT --masterAccount $MASTER_ACCOUNT --initialBalance 20
-
-near deploy $SUB_ACCOUNT --wasmFile=./res/wishlist.wasm
